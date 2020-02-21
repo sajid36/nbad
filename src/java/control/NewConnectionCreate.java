cf5042a82a148fcd51d0487e64ec6cb259baeb9d
sajid36 <sajidulislam52@yahoo.com>
2020-02-21 03:58:45 -0500

    name changed

--- src/java/control/NewConnectionCreate.java
+++ src/java/control/NewConnectionCreate.java
@@ -90,7 +90,6 @@ public class NewConnectionCreate extends HttpServlet {
         //request for all connections
         String action = null;
         action = request.getParameter("connectionId");
-        
         //request for single connection
         if(action == null || action.isEmpty()){
             //System.out.println(action);
